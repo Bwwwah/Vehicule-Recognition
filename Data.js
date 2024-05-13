1d46502086dc16f514cada1e6e4522dc6b81fbbe
Bwwwah <d.brocard06@icloud.com>
2024-05-14 00:16:45 +0200

    feat(data): add data recupération

---
+++ Data.js
@@ -0,0 +1,40 @@
+import fetch from 'node-fetch';
+import crypto from 'crypto';
+import fs from 'fs';
+
+let previousHash = '';
+let i = 1;
+
+async function downloadImages() {
+  if (!fs.existsSync('Images')) {
+    fs.mkdirSync('Images');
+  }
+
+  for (let j = 0; j < 50; j++) {
+    let hash;
+    let buffer;
+
+    while (true) {
+      console.log("Downloading image...")
+      const response = await fetch('https://download.data.grandlyon.com/files/rdata/pvo_patrimoine_voirie.pvocameracriter/CWL9018.JPG');
+      buffer = await response.buffer();
+      hash = crypto.createHash('sha256').update(buffer).digest('hex');
+
+      if (hash !== previousHash) {
+        break;
+      }
+
+      console.log('Hash is identical, waiting 10 seconds...');
+      await new Promise(resolve => setTimeout(resolve, 10 * 1000)); // 10 * 1000 ms = 10 seconds
+    }
+
+    fs.writeFileSync(`Images/image_${i}.jpg`, buffer);
+    console.log(i + ' image downloaded');
+    i++;
+    previousHash = hash;
+
+    await new Promise(resolve => setTimeout(resolve, 60 * 1000)); // 60 * 1000 ms = 1 minute
+  }
+}
+
+downloadImages();

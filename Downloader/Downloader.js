8df1a4a15a1d99cd242ecc8b68bc321ee4d35259
Bwwwah <d.brocard06@icloud.com>
2024-05-15 16:17:49 +0200

    fix timestamp

--- Downloader/Downloader.js
+++ Downloader/Downloader.js
@@ -28,7 +28,18 @@ async function downloadImages() {
       await new Promise(resolve => setTimeout(resolve, 10 * 1000)); // 10 * 1000 ms = 10 seconds
     }
 
-    fs.writeFileSync(`/Data/image_${i}.jpg`, buffer);
+    function getFormattedTimestamp() {
+      const date = new Date();
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, '0');
+      const day = String(date.getDate()).padStart(2, '0');
+      const hours = String(date.getHours()).padStart(2, '0');
+      const minutes = String(date.getMinutes()).padStart(2, '0');
+      const seconds = String(date.getSeconds()).padStart(2, '0');
+      return `${year}${month}${day}${hours}${minutes}${seconds}`;
+    }
+
+    fs.writeFileSync(`/Data/${getFormattedTimestamp()}.jpg`, buffer);
     console.log(i + ' image downloaded');
     i++;
     previousHash = hash;
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Vehicule-Recognition
 
 docker build -t downloader ./Downloader
-docker run -d --rm -v ./Downloader/Data:/Data downloader 
+docker run -d --rm -v ./Downloader/Data:/Data --name Downloader downloader
 
 docker build -t dataset -f ./Dataset/dockerfile .
-docker run -d --rm -v ./Dataset/Images:/Images dataset
+docker run -d --rm -v ./Dataset/Images:/Images --name Dataset dataset
